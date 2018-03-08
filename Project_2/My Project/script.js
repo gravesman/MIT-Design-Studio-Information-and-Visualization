a5ac46c32378e13754576aa3a0a9455ceb959d87
gravesman <36311645+gravesman@users.noreply.github.com>
2018-03-08 00:55:51 -0500

    Updated dark sky api link

--- Project_2/My Project/script.js
+++ Project_2/My Project/script.js
@@ -3,7 +3,7 @@
 
 
 $.ajax({
-  url: 'https://api.darksky.net/forecast/c6b293fcd2092b65cfb7313424b2f7ff/42.361145,-71.057083',
+  url: 'https://api.darksky.net/forecast/c63bd23e376113970b4fc4ba50ef034b/42.361145,-71.057083',
   dataType: 'JSONP',
   type: 'GET',
   crossDomain: true,
